ee823237c95738e74bff6706ce11c638c825b92d
violet313 <violet313@gmail.com>
2017-10-27 21:56:30 +0100

    Woot. No we're getting somewhere. Added a TableLayoutPanel1 with props of Autosize=true, Dock=Fill. So now we have a fixed-size header and dynamic content with scrollbars when necessary.

--- TestWinFormsApplication/FormTest1.Designer.vb
+++ TestWinFormsApplication/FormTest1.Designer.vb
@@ -24,7 +24,9 @@ Partial Class FormTest1
     Private Sub InitializeComponent()
         Me.LabelMsg = New System.Windows.Forms.Label()
         Me.PanelBody = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.PanelBody.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'LabelMsg
@@ -42,10 +44,25 @@ Partial Class FormTest1
         Me.PanelBody.AutoSize = True
         Me.PanelBody.Controls.Add(Me.LabelMsg)
         Me.PanelBody.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PanelBody.Location = New System.Drawing.Point(0, 0)
+        Me.PanelBody.Location = New System.Drawing.Point(3, 53)
         Me.PanelBody.Name = "PanelBody"
-        Me.PanelBody.Size = New System.Drawing.Size(310, 162)
+        Me.PanelBody.Size = New System.Drawing.Size(626, 106)
         Me.PanelBody.TabIndex = 1
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.AutoSize = True
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PanelBody, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(632, 162)
+        Me.TableLayoutPanel1.TabIndex = 2
         '
         'FormTest1
         '
@@ -53,15 +70,17 @@ Partial Class FormTest1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(310, 162)
-        Me.Controls.Add(Me.PanelBody)
+        Me.ClientSize = New System.Drawing.Size(632, 162)
+        Me.Controls.Add(Me.TableLayoutPanel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.MaximumSize = New System.Drawing.Size(800, 200)
+        Me.MaximumSize = New System.Drawing.Size(800, 300)
         Me.MinimumSize = New System.Drawing.Size(100, 100)
         Me.Name = "FormTest1"
         Me.Text = "FormTest1"
         Me.PanelBody.ResumeLayout(False)
         Me.PanelBody.PerformLayout()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -69,4 +88,5 @@ Partial Class FormTest1
 
     Friend WithEvents LabelMsg As Label
     Friend WithEvents PanelBody As Panel
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
